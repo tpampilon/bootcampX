08b233c0bac66f30f44740c820d6829a9b3a1423
TedP <ted.pampilon@gmail.com>
2021-03-30 21:39:21 +0000

    add 1_total_student_assistance_requests.sql

--- 4_queries/1_total_student_assistance_requests.sql
+++ 4_queries/1_total_student_assistance_requests.sql
@@ -1,4 +1,7 @@
 -- 1_total_student_assistance_requests
 
-SELECT
-FROM
+SELECT count(assistance_requests.*) as total_assistances, students.name
+FROM assistance_requests
+JOIN students ON students.id = student_id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
