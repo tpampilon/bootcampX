00a190ee101c4352fe605cd955f049ca9c6c07a4
TedP <ted.pampilon@gmail.com>
2021-03-30 21:51:55 +0000

    add 2_assistance_requests_data.sql

--- 4_queries/2_assistance_requests_data.sql
+++ 4_queries/2_assistance_requests_data.sql
@@ -1,4 +1,8 @@
 -- 2_assistance_requests_data
 
-SELECT
-FROM
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN assignments ON assignments.id = assignment_id
+ORDER BY duration;
