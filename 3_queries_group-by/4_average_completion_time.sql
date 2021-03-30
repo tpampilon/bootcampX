f834a72f1f8c31999475484361a978d46daede44
TedP <ted.pampilon@gmail.com>
2021-03-30 05:09:10 +0000

    add 4_average_completion_time.sql

---
+++ 3_queries_group-by/4_average_completion_time.sql
@@ -0,0 +1,8 @@
+-- 4_average_completion_time
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
