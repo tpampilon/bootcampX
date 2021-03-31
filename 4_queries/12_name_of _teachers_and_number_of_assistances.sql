883360b0ff041adc88e0553041abd89bc9e76dfa
TedP <ted.pampilon@gmail.com>
2021-03-31 00:33:54 +0000

    add 12_name_of_teachers_and_number_of_assistances.sql

--- 4_queries/12_name_of _teachers_and_number_of_assistances.sql
+++
@@ -1,4 +0,0 @@
--- 12_name_of _teachers_and_number_of_assistances
-
-SELECT
-FROM
---
+++ 4_queries/12_name_of_teachers_and_number_of_assistances.sql
@@ -0,0 +1,10 @@
+-- 12_name_of_teachers_and_number_of_assistances
+
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
