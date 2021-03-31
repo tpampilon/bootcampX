c989a6f4bfc149b7d579e2622877688ed8ecb994
TedP <ted.pampilon@gmail.com>
2021-03-31 00:06:53 +0000

    add 9_most_confusing_assignments.sql

--- 4_queries/9_most_confusing_assignments.sql
+++ 4_queries/9_most_confusing_assignments.sql
@@ -1,4 +1,7 @@
 -- 9_most_confusing_assignments
 
-SELECT
-FROM
+SELECT assignments.id, name, day, chapter, count(assistance_requests) as total_requests
+FROM assignments
+JOIN assistance_requests ON assignments.id = assignment_id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
